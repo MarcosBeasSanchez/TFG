18b016a4d0f2bd057f060e021275b5b9162575f2
MarcosBeasSanchez <mbs441@educa.madrid.org>
2025-07-02 13:55:39 +0200

    resto de los datos de Java con Sprigboot

---
+++ src/test/java/tfg/proyecto/TFG/ApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.proyecto.TFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

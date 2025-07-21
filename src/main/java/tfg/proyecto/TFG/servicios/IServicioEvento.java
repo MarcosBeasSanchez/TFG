4c451de2f8561e2ff3210ddc468c58296a6c0c28
DylanMolinaAmariles <dylanmolina1973@gmail.com>
2025-07-21 18:31:02 +0200

    servicios de ticket evento hechos falta cuentaBancaria

--- src/main/java/tfg/proyecto/TFG/servicios/IServicioEvento.java
+++ src/main/java/tfg/proyecto/TFG/servicios/IServicioEvento.java
@@ -1,5 +1,19 @@
 package tfg.proyecto.TFG.servicios;
 
-public interface IServicioEvento {
+import java.util.List;
 
+import tfg.proyecto.TFG.dtos.DTOeventoBajada;
+import tfg.proyecto.TFG.dtos.DTOeventoSubida;
+
+public interface IServicioEvento {
+	
+	DTOeventoBajada insert(DTOeventoSubida dto);
+	
+	List<DTOeventoBajada> obtenerTodosLosEventos();
+
+	DTOeventoBajada actualizarEvento(Long id,  DTOeventoSubida eventodto);
+	
+	boolean eliminarEvento(Long id);
+	
+	DTOeventoBajada obtnerPorElNombre(String nombre);
 }
--- src/main/java/tfg/proyecto/TFG/servicios/IServicioUsuario.java
+++ src/main/java/tfg/proyecto/TFG/servicios/IServicioUsuario.java
@@ -1,14 +1,13 @@
 package tfg.proyecto.TFG.servicios;
 
-<<<<<<< HEAD
-=======
+
 import java.util.List;
 
 import tfg.proyecto.TFG.dtos.DTOusuarioBajada;
 import tfg.proyecto.TFG.dtos.DTOusuarioModificarSubida;
 import tfg.proyecto.TFG.dtos.DTOusuarioSubida;
 
->>>>>>> 7bf72fd3a1c13ce7e42151cbc007e26d501b08b5
+
 public interface IServicioUsuario {
 	
 	DTOusuarioBajada insert(DTOusuarioSubida usuarioDto);
@@ -16,8 +15,5 @@ public interface IServicioUsuario {
 	Integer deleteById(Long id); //eliminar usuario por id
 	DTOusuarioBajada findById(Long id);
 	List<DTOusuarioBajada>findAllUsuarios();
-	
-	
-	
 
 }
